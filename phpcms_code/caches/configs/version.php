8f5fbce0ce8ff8674ccef3f614ff6275f047a23c
Abel-lan <abel-blue@outlook.com>
2021-04-19 11:35:15 +0800

    修改版本号

--- phpcms_code/caches/configs/version.php
+++ phpcms_code/caches/configs/version.php
@@ -1,6 +1,6 @@
 <?php
 return array(
-'pc_version' => 'V9.7.0',	//phpcms 版本号
-'pc_release' => '20210416',	//phpcms 更新日期
+'pc_version' => 'V9.7.1',	//phpcms 版本号
+'pc_release' => '20210419',	//phpcms 更新日期
 );
 ?>
